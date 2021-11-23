923c344cfff81d810cbfb8e7123161cc789a02f1
Itamar Junior <itjunior@gmail.com>
2021-11-23 17:47:11 +0000

    .commit

---
+++ Y2S1Object-OrientedProgramming-CarRental/models/CarRental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Y2S1ObjectOrientedProgrammingCarRental.models
+{
+    public class CarRental
+    {
+        public int rentalID { get;set; }
+        public string carModel { get; set; }
+        public string carRegistration { get; set; }
+
+        public CarRental(int _rentalID, string _carModel, string _carRegistration)
+        {
+            this.rentalID = _rentalID;
+            this.carModel = _carModel;
+            this.carRegistration = _carRegistration;
+        }
+
+        public CarRental()
+        {
+
+        }
+
+    }
+}
---
+++ Y2S1Object-OrientedProgramming-CarRental/models/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Y2S1ObjectOrientedProgrammingCarRental.models
+{
+    public class Driver
+    {
+        public List<CarRental> ListOfCarRentals = new List<CarRental>()
+        {
+
+            new CarRental(1, "Ford", "a1234"),
+            new CarRental(2, "Opel", "b4557"),
+            new CarRental(3, "Nissan", "c891")
+        };
+
+        public void displayCarRental()
+        {
+            foreach (CarRental car in ListOfCarRentals)
+            {
+                Console.WriteLine($"{car.rentalID}, {car.carModel}, {car.carRegistration}");
+
+            }
+        }
+    }
+}
