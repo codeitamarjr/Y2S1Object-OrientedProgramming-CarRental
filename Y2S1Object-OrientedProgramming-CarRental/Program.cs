3eaf8e6da66fbfeeba713e1afceafd8f7dd09bd3
Itamar Junior <itjunior@gmail.com>
2021-11-23 17:46:08 +0000

    first commit.

---
+++ Y2S1Object-OrientedProgramming-CarRental/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Y2S1ObjectOrientedProgrammingCarRental.models;
+namespace Y2S1ObjectOrientedProgrammingCarRental
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            
+
+
+        }
+    }
+}
